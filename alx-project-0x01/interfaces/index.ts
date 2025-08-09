f63a05aaabc793e66a84c55c219bfbd07d1e4761
Sebrin <sebrinmudesir@gmail.com>
2025-08-09 19:37:04 +0300

    Created a post form that allows users to add posts and linked it to the add posts button on the posts page.

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,4 +1,14 @@
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
 
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
 
 export interface Geo {
   lat: string;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
+import Header from "@/components/layout/Header";
 
 interface UsersPageProps {
   posts: UserProps[];  // prop name must be 'posts' as per getStaticProps
@@ -10,6 +11,7 @@ interface UsersPageProps {
 const Users: React.FC<UsersPageProps> = ({ posts }) => {
   return (
     <div className="flex flex-col min-h-screen p-4 bg-gray-50">
+      <Header/>
       <h1 className="text-3xl font-bold mb-4">Users</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {/* Map over posts to dynamically render UserCard components */}
